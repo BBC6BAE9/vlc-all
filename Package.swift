451ccae5a4240d38bf827fc2123465b1c27e370d
BBC6BAE9 <bbc6bae9@gmail.com>
2025-03-14 12:38:45 +0800

    initial commit

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let vlcBinary = Target.binaryTarget(name: "VLCKit-all", url: "https://github.com/BBC6BAE9/vlc-all/releases/download/4.0.0a10/VLCKit.xcframework.zip", checksum: "")
+
+let package = Package(
+    name: "vlc-all",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "vlc-all",
+            targets: ["vlc-all"]),
+    ],
+    targets: [
+        vlcBinary,
+        .target(
+            name: "vlc-all",
+            dependencies: [
+                .target(name: "VLCKit-all")
+            ], linkerSettings: [
+                .linkedFramework("QuartzCore", .when(platforms: [.iOS])),
+                .linkedFramework("CoreText", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("AVFoundation", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("Security", .when(platforms: [.iOS])),
+                .linkedFramework("CFNetwork", .when(platforms: [.iOS])),
+                .linkedFramework("AudioToolbox", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("OpenGLES", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("CoreGraphics", .when(platforms: [.iOS])),
+                .linkedFramework("VideoToolbox", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("CoreMedia", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("c++", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("xml2", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("z", .when(platforms: [.iOS, .tvOS])),
+                .linkedLibrary("bz2", .when(platforms: [.iOS, .tvOS])),
+                .linkedFramework("Foundation", .when(platforms: [.macOS])),
+                .linkedLibrary("iconv")
+            ]),
+    ]
+)
